dfe29384966b4e291087ab31df8a7402b6e1a9e1
caojiayuan <caojiayuan@newangels.cn>
2018-01-04 16:18:41 +0800

    定时器 年月周天时间 分别按需求计算 并执行各自操作

--- controller/src/main/java/org/building/pm/timingTask/hpTimer.java
+++ controller/src/main/java/org/building/pm/timingTask/hpTimer.java
@@ -32,6 +32,7 @@ public class hpTimer {
         List<Map<String, Object>> dataList = (List<Map<String, Object>>) data.get("list");
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
+        long nw = 1000 * 24 * 60 * 60 * 7;//天
         long nd = 1000 * 24 * 60 * 60;//天
         long nh = 1000 * 60 * 60;//小时
         long nm = 1000 * 60;//分钟
@@ -62,6 +63,15 @@ public class hpTimer {
             long hour=0;
             int hourc=0;
 
+            long day=0;
+            int dayc=0;
+
+            long week=0;
+            int weekc=0;
+
+            int month=0;
+            int monthc=0;
+
             if (timetype.equals("小时")) {
                 diff = nowdate.getTime() - plandate.getTime();// 获得两个时间的毫秒时间差异
                 hour = diff  / nh;//相差小时数
@@ -73,12 +83,103 @@ public class hpTimer {
                     hpService.PM_06_DJ_DATA_TIMER_SET((String) dataList.get(i).get("V_GUID"), v_v_timer_guid, (String) dataList.get(i).get("V_FZ_PER"), (String) dataList.get(i).get("V_DJ_TYPE"), format.format(timeer));
                 }
             } else if (timetype.equals("天")) {
+                diff = nowdate.getTime() - plandate.getTime();// 获得两个时间的毫秒时间差异
+                day = diff  / nd;//相差天数
+                dayc = (int) (day / zq);//相差周期数
+
+                for (int j = 1; j <= dayc; j++) {
+                    long timeer=plandate.getTime()+j*zq*nd;
+
+                    hpService.PM_06_DJ_DATA_TIMER_SET((String) dataList.get(i).get("V_GUID"), v_v_timer_guid, (String) dataList.get(i).get("V_FZ_PER"), (String) dataList.get(i).get("V_DJ_TYPE"), format.format(timeer));
+                }
 
             } else if (timetype.equals("周")) {
 
+                diff = nowdate.getTime() - plandate.getTime();// 获得两个时间的毫秒时间差异
+                week = diff  / nw;//相差周数
+                weekc = (int) (week / zq);//相差周期数
+
+                for (int j = 1; j <= weekc; j++) {
+                    long timeer=plandate.getTime()+j*zq*nw;
+
+                    hpService.PM_06_DJ_DATA_TIMER_SET((String) dataList.get(i).get("V_GUID"), v_v_timer_guid, (String) dataList.get(i).get("V_FZ_PER"), (String) dataList.get(i).get("V_DJ_TYPE"), format.format(timeer));
+                }
+
             } else if (timetype.equals("月")) {
+                Calendar cnowdate = Calendar.getInstance();
+                Calendar cplandate = Calendar.getInstance();
+                cnowdate.setTime(nowdate);
+                cplandate.setTime(plandate);
+                if(cnowdate.getTimeInMillis() < cplandate.getTimeInMillis()){
+                    //现日期早于计划日期，无法添加
+                }
+                int yearn = cnowdate.get(Calendar.YEAR);
+                int yearp = cplandate.get(Calendar.YEAR);
+                int monthn = cnowdate.get(Calendar.MONTH);
+                int monthp = cplandate.get(Calendar.MONTH);
+                int dayn = cnowdate.get(Calendar.DAY_OF_MONTH);
+                int dayp = cplandate.get(Calendar.DAY_OF_MONTH);
+
+                // 获取年的差值
+                int yearInterval = yearn - yearp;
+
+                // 如果 nowdate 月-日 小于 plandate 月-日 那么 yearInterval-- 这样就得到了相差的年数
+                if(monthn < monthp || monthn == monthp && dayn < dayp)
+                {
+                    yearInterval --;
+                }
+                // 获取月数差值
+                int monthInterval =  (monthn + 12) - monthp  ;
+
+                if(dayn < dayp){
+                    monthInterval --;
+                }
+                monthInterval %= 12;
+
+                month = yearInterval * 12 + monthInterval;//相差月数
+                monthc=month/zq;//相差周期数
+
+                for (int j = 1; j <= monthc; j++) {
+
+                    int yearzq=yearp;
+                    int monthzq=monthp+zq*j;
+                    if(monthzq>12){
+                        yearzq++;
+                        monthzq %=12;
+                    }
+
+                    Date date=new Date(yearzq+"-"+monthzq+"-"+dayp);
+                    long timeer=date.getTime();
+
+                    hpService.PM_06_DJ_DATA_TIMER_SET((String) dataList.get(i).get("V_GUID"), v_v_timer_guid, (String) dataList.get(i).get("V_FZ_PER"), (String) dataList.get(i).get("V_DJ_TYPE"), format.format(timeer));
+                }
 
             } else if (timetype.equals("年")) {
+
+                Calendar cnowdate = Calendar.getInstance();
+                Calendar cplandate = Calendar.getInstance();
+                cnowdate.setTime(nowdate);
+                cplandate.setTime(plandate);
+                int yearn = cnowdate.get(Calendar.YEAR);
+                int yearp = cplandate.get(Calendar.YEAR);
+                int monthn = cnowdate.get(Calendar.MONTH);
+                int monthp = cplandate.get(Calendar.MONTH);
+                int dayn = cnowdate.get(Calendar.DAY_OF_MONTH);
+                int dayp = cplandate.get(Calendar.DAY_OF_MONTH);
+
+
+                int year=yearn-yearp;
+                int yearc=year/zq;
+
+                for(int j = 1; j <= monthc; j++){
+
+                    int yearzq=yearp+j*zq;
+                    Date date=new Date(yearzq+"-"+monthp+"-"+dayp);
+                    long timeer=date.getTime();
+
+                    hpService.PM_06_DJ_DATA_TIMER_SET((String) dataList.get(i).get("V_GUID"), v_v_timer_guid, (String) dataList.get(i).get("V_FZ_PER"), (String) dataList.get(i).get("V_DJ_TYPE"), format.format(timeer));
+
+                }
 
             }
         }
