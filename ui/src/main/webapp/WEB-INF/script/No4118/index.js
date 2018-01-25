d9b471f3605bba950071f4ca2c40a70bca7d5987
张迹海 <zhangjihai@newangels.cn>
2018-01-25 14:10:58 +0800

    工单查询修改

--- ui/src/main/webapp/WEB-INF/script/No4118/index.js
+++ ui/src/main/webapp/WEB-INF/script/No4118/index.js
@@ -26,49 +26,6 @@ Ext.onReady(function() {
         }
     });
 
-
-   /* var ckstore = Ext.create("Ext.data.Store", {
-        autoLoad : true,
-        storeId : 'ckstore',
-        fields : [ 'V_DEPTCODE', 'V_DEPTNAME' ],
-        proxy : {
-            type : 'ajax',
-            async : false,
-            url: AppUrl + 'PM_06/PRO_BASE_DEPT_VIEW_ROLE',
-            actionMethods : {
-                read : 'POST'
-            },
-            reader : {
-                type : 'json',
-                root : 'list'
-            },
-            extraParams : {
-                'V_V_PERSONCODE': Ext.util.Cookies.get('v_personcode'),
-                'V_V_DEPTCODE':  Ext.util.Cookies.get('v_orgCode'),
-                'V_V_DEPTCODENEXT': Ext.util.Cookies.get('v_deptcode'),
-                'V_V_DEPTTYPE': '基层单位'
-            }
-        }
-    });
-
-    var zyqstore = Ext.create("Ext.data.Store", {
-        autoLoad : false,
-        storeId : 'zyqstore',
-        fields : [ 'V_DEPTCODE', 'V_DEPTNAME' ],
-        proxy : {
-            type : 'ajax',
-            async : false,
-            url: AppUrl + 'PM_06/PRO_BASE_DEPT_VIEW_ROLE',
-            actionMethods : {
-                read : 'POST'
-            },
-            reader : {
-                type : 'json',
-                root : 'list'
-            }
-        }
-    });*/
-
     var sgdzt = Ext.create("Ext.data.Store", {
         autoLoad : false,
         storeId : 'sgdzt',
@@ -87,60 +44,6 @@ Ext.onReady(function() {
         }
     });
 
-    /*var ssblx = Ext.create('Ext.data.Store', {
-        autoLoad : false,
-        storeId : 'ssblx',
-        fields : [ 'V_EQUTYPECODE', 'V_EQUTYPENAME' ],
-        proxy : {
-            type : 'ajax',
-            async : false,
-            url : AppUrl + 'WorkOrder/PRO_GET_DEPTEQUTYPE_ADMIN',
-            actionMethods : {
-                read : 'POST'
-            },
-            reader : {
-                type : 'json',
-                root : 'list'
-            }
-        }
-    });
-
-    var ssbmc = Ext.create('Ext.data.Store', {
-        autoLoad : false,
-        storeId : 'ssbmc',
-        fields : [ 'V_EQUCODE', 'V_EQUNAME' ],
-        proxy : {
-            type : 'ajax',
-            async : false,
-            url : AppUrl + 'WorkOrder/PRO_GET_DEPTEQU_ADMIN',
-            actionMethods : {
-                read : 'POST'
-            },
-            reader : {
-                type : 'json',
-                root : 'list'
-            }
-        }
-    });
-
-    var sdjy = Ext.create('Ext.data.Store', {
-        autoLoad : false,
-        storeId : 'sdjy',
-        fields : [ 'V_PERSONCODE', 'V_PERSONNAME' ],
-        proxy : {
-            type : 'ajax',
-            async : false,
-            url : AppUrl + 'No4120/PRO_BASE_PERSON_VIEW_ROLE',
-            actionMethods : {
-                read : 'POST'
-            },
-            reader : {
-                type : 'json',
-                root : 'list'
-            }
-        }
-    });*/
-
     var panel =Ext.create('Ext.panel.Panel',{
         id : 'panellow',
         region : 'north',
@@ -150,16 +53,7 @@ Ext.onReady(function() {
             style: 'margin:5px 0px 5px 5px',
             labelAlign: 'right'
         },
-        items : [/*{id : 'begintime',xtype : 'datefield',editable : false,format : 'Y/m/d',value : new Date(new Date().getFullYear() + '/'+ (new Date().getMonth() + 1) + '/' + 1),fieldLabel : '时间段选择',labelWidth : 80, baseCls : 'margin-bottom'},
-                 { id : 'endtime',xtype : 'datefield',editable : false,format : 'Y/m/d',value : new Date(),fieldLabel : '至',labelWidth : 80},
-                 {id : 'ck',xtype : 'combo',store : ckstore,editable : false,fieldLabel : '厂矿',labelWidth : 80,displayField : 'V_DEPTNAME', valueField : 'V_DEPTCODE',queryMode : 'local', baseCls : 'margin-bottom' },
-                 {id : 'zyq',xtype : 'combo',store : zyqstore,editable : false,fieldLabel : '作业区',labelWidth : 80, displayField : 'V_DEPTNAME',valueField : 'V_DEPTCODE',queryMode : 'local', baseCls : 'margin-bottom'},*/
-                 {id : 'gdzt',xtype : 'combo',store : sgdzt,editable : false, fieldLabel : '工单状态',labelWidth : 80,displayField : 'V_STATENAME', valueField : 'V_STATECODE', queryMode : 'local', baseCls : 'margin-bottom'},
-                 /*{id : 'sblx',xtype : 'combo', store : ssblx,editable : false,fieldLabel : '设备类型', labelWidth : 80,displayField : 'V_EQUTYPENAME',valueField : 'V_EQUTYPECODE',queryMode : 'local', baseCls : 'margin-bottom'},
-                 {id : 'sbmc',xtype : 'combo', store : ssbmc,editable : false,fieldLabel : '设备名称', labelWidth : 80,displayField : 'V_EQUNAME',valueField : 'V_EQUCODE',queryMode : 'local', baseCls : 'margin-bottom'},
-                 {id : 'djy',xtype : 'combo', store : sdjy,editable : false,fieldLabel : '点检员', labelWidth : 80,displayField : 'V_PERSONNAME',valueField : 'V_PERSONCODE',queryMode : 'local', baseCls : 'margin-bottom'},
-                {id : 'selshortTxt',xtype : 'textfield', width : 158,emptyText : '按工单描述模糊搜索',margin:'5px 0px 5px 90px'},
-                {id : 'selmatDesc',xtype : 'textfield', width : 158,emptyText : '按使用物料模糊搜索',margin:'5px 0px 5px 90px'},*/
+        items : [{id : 'gdzt',xtype : 'combo',store : sgdzt,editable : false, fieldLabel : '工单状态',labelWidth : 80,displayField : 'V_STATENAME', valueField : 'V_STATECODE', queryMode : 'local', baseCls : 'margin-bottom'},
                 {id : 'query',xtype : 'button', icon : '../../images/gif/search.png',text : '查询', width : 80,listeners: {click: QueryGrid}},
                 { xtype : 'button',text : '导出excel',icon : '../../images/gif/grid.png',width : 85, listeners : { click : OnClickExcelButton}},
                 { xtype : 'hidden',id : 'tabid'}]
@@ -290,8 +184,6 @@ Ext.onReady(function() {
         activeTab: 0,
         listeners: {
             tabchange: function () {
-                /*Ext.getCmp('page').store.currentPage = 1;
-                gridStore.load();*/
                 if (querytool) {
                     Ext.ComponentManager.get("tabid").setValue(Ext
                         .getCmp('tabpanel').getActiveTab().down("hidden")
@@ -311,61 +203,12 @@ Ext.onReady(function() {
     Ext.create('Ext.container.Viewport', {
         id : "id",
         layout : 'border',
-        items : [panel,/*{xtype:'panel',id:'testPanel',region:'north',height:40},*/grid]
+        items : [panel,grid]
     });
+    QueryGrid();
 
-   /* Ext.data.StoreManager.lookup('ckstore').on("load", function() {
-        Ext.getCmp("ck").select(Ext.data.StoreManager.lookup('ckstore').getAt(0));
-        Ext.data.StoreManager.lookup('zyqstore').load({
-            params : {
-                'V_V_PERSONCODE': Ext.util.Cookies.get('v_personcode'),
-                'V_V_DEPTCODE':   Ext.getCmp("ck").getValue(),
-                'V_V_DEPTCODENEXT':  Ext.util.Cookies.get('v_deptcode'),
-                'V_V_DEPTTYPE':'[主体作业区]'
-            }
-        });
-    });
 
-    Ext.data.StoreManager.lookup('zyqstore').on("load", function() {
-        Ext.getCmp("zyq").select(Ext.data.StoreManager.lookup('zyqstore').getAt(0));
-        Ext.data.StoreManager.lookup('ssblx').load({
-            params : {
-                V_V_DEPTCODENEXT:Ext.getCmp("zyq").getValue()
-            }
-        });
-    });
-
-    Ext.data.StoreManager.lookup('ssblx').on('load', function() {
-        Ext.getCmp("sblx").select(Ext.data.StoreManager.lookup('ssblx').getAt(0));
-        Ext.data.StoreManager.lookup('ssbmc').load({
-            params : {
-                V_V_DEPTCODENEXT :Ext.getCmp("zyq").getValue(),
-                V_V_EQUTYPECODE : Ext.getCmp("sblx").getValue()
-            }
-        });
-
-    });
-
-    Ext.data.StoreManager.lookup('ssbmc').on('load', function() {
-        Ext.getCmp("sbmc").select(Ext.data.StoreManager.lookup('ssbmc').getAt(0));
-        Ext.data.StoreManager.lookup('sdjy').load({
-            params : {
-                V_V_DEPTCODE :Ext.getCmp("zyq").getValue(),
-                V_V_PERSONCODE : Ext.util.Cookies.get('v_personcode'),
-                V_V_ROLE: '01'
-            }
-        });
-    });*/
-
-    //Ext.data.StoreManager.lookup('sdjy').on('load', function() {
-    //    /*Ext.ComponentManager.get('djy').store.insert(0, {
-    //        'V_PERSONCODE' : '%',
-    //        'V_PERSONNAME' : '全部'
-    //    });*/
-    //    Ext.getCmp('djy').select(Ext.data.StoreManager.lookup('sdjy').getAt(0));
-
-        Ext.data.StoreManager.lookup('sgdzt').load({});
-   // });
+    Ext.data.StoreManager.lookup('sgdzt').load({});
 
     Ext.data.StoreManager.lookup('sgdzt').on("load", function() {
         Ext.ComponentManager.get('gdzt').store.insert(0, {
@@ -373,59 +216,10 @@ Ext.onReady(function() {
             'V_STATENAME' : '全部'
         });
         Ext.getCmp("gdzt").select(Ext.data.StoreManager.lookup('sgdzt').getAt(0));
-        /*Ext.getCmp("testPanel").add(tabpanel);
-        if(tabtool){
-            addTab();
-        }*/
-
     });
 
-   /* Ext.getCmp("ck").on("select",function() {
-        tabtool=false;
-        Ext.getCmp('zyq').getStore().removeAll();
-        Ext.data.StoreManager.lookup('zyqstore').load({
-             params : {
-                 V_V_PERSONCODE : Ext.util.Cookies.get('v_personcode'),
-                 V_V_DEPTCODE:Ext.getCmp("ck").getValue(),
-                 V_V_DEPTCODENEXT: Ext.util.Cookies.get('v_deptcode'),
-                 V_V_DEPTTYPE:'[主体作业区]'
-                }
-            });
-        });
-
-    Ext.getCmp('zyq').on('select', function() {
-        tabtool=false;
-        Ext.getCmp('sblx').getStore().removeAll();
-        Ext.getCmp('djy').getStore().removeAll();
-        Ext.data.StoreManager.lookup('ssblx').load({
-                params : {
-                    V_V_DEPTCODENEXT : Ext.getCmp("zyq").getValue()
-                }
-            });
-
-        Ext.data.StoreManager.lookup('sdjy').load({
-                params : {
-                    V_V_DEPTCODE : Ext.getCmp("zyq").getValue(),
-                    V_V_PERSONCODE : Ext.util.Cookies.get('v_personcode'),
-                    V_V_ROLE :'01'
-                }
-            });
-        });
-
-    Ext.getCmp('sblx').on( 'select',function() {
-        tabtool=false;
-        Ext.getCmp('sbmc').getStore().removeAll();
-        Ext.data.StoreManager.lookup('ssbmc').load({
-                params : {
-                    V_V_DEPTCODENEXT :Ext.getCmp("zyq").getValue(),
-                    V_V_EQUTYPECODE : Ext.getCmp("sblx").getValue()
-                }
-            });
-        });*/
-
-
-    Ext.data.StoreManager.lookup('gridStore').load({
-        params: {
+    Ext.data.StoreManager.lookup('gridStore').on('beforeload', function (store) {
+        store.proxy.extraParams = {
             V_D_ENTER_DATE_B : '%',
             V_D_ENTER_DATE_E : '%',
             V_V_ORGCODE : '%',
@@ -442,7 +236,7 @@ Ext.onReady(function() {
             V_V_PAGESIZE: Ext.getCmp('page').store.pageSize
         }
     });
-    //addTab();
+
 });
 function OnSearch() {
     querytool = false;
@@ -485,19 +279,26 @@ function OnSearch() {
         }
     });
 
-
 }
 function QueryGrid() {
-
-    /*tabIndex = parseInt(Ext.getCmp('tabpanel').getActiveTab().id.substring(8));
-    OnSearch();
-
-    Ext.ComponentManager.get("tabpanel").setActiveTab(tabIndex);*/
-
-   // if(Ext.getCmp('tabpanel').getActiveTab().id.substring(8)==0){
-        Ext.getCmp('page').store.currentPage = 1;
-        Ext.data.StoreManager.lookup('gridStore').load();
-    //}
+    Ext.data.StoreManager.lookup('gridStore').load({
+        params: {
+            V_D_ENTER_DATE_B : '%',
+            V_D_ENTER_DATE_E : '%',
+            V_V_ORGCODE : '%',
+            V_V_DEPTCODE : '%',
+            V_V_DEPTCODEREPARIR :'',
+            V_V_STATECODE : Ext.getCmp( "gdzt").getValue(),
+            V_EQUTYPE_CODE : '%',
+            V_EQU_CODE : '%',
+            V_DJ_PERCODE :Ext.util.Cookies.get('v_personcode'),
+            V_V_SHORT_TXT : '%',
+            V_V_BJ_TXT : '%',
+            V_V_ORDER_TYP : '%',
+            V_V_PAGE: Ext.getCmp('page').store.currentPage,
+            V_V_PAGESIZE: Ext.getCmp('page').store.pageSize
+        }
+    });
 }
 function rendererZGD(a, value, metaData){
     return '<a href="javascript:goToZGD(\'' + metaData.data.V_ORDERGUID + '\')">' + a + '</a>';
