c207b19147504ec39f77045b2640f252e0244b13
caojiayuan <caojiayuan@newangels.cn>
2018-03-06 09:51:48 +0800

    22010101 样式调整

--- ui/src/main/webapp/WEB-INF/script/PM_220101/index.js
+++ ui/src/main/webapp/WEB-INF/script/PM_220101/index.js
@@ -298,8 +298,8 @@ function _newapplication() {
 
     V_GUID = Ext.data.IdGenerator.get('uuid').generate();
 
-    var owidth = window.document.body.offsetWidth - 200;
-    var oheight = window.document.body.offsetHeight - 100;
+    var owidth = window.document.body.offsetWidth;
+    var oheight = window.document.body.offsetHeight;
     window.open(AppUrl + 'page/PM_22010101/index.html?V_GUID=' + V_GUID  + '&random=' + Math.random(), '', 'height=' + oheight + ',width=' + owidth + ',top=10px,left=10px,resizable=no' );
     // var matStockLevel = window.showModalDialog(AppUrl + 'page/PM_140701/index.html?IN_DEPARTCODE=' + Ext.getCmp("zyq").getValue() + '&V_V_GUID=' + records[0].get("V_GUID") + '&random=' + Math.random(), window, 'resizable=yes;  dialogWidth=1200px; dialogHeight=1000px');
     /*if (b) {
