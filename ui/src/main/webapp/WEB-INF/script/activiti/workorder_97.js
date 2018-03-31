a77a766c5c73b61b26f8a7d916db2a5422727279
caojiayuan <caojiayuan@newangels.cn>
2018-03-31 08:40:49 +0800

    091105 workorder97 生成模型 过程 功能 后台

--- ui/src/main/webapp/WEB-INF/script/activiti/workorder_97.js
+++ ui/src/main/webapp/WEB-INF/script/activiti/workorder_97.js
@@ -1386,7 +1386,10 @@ function OnClickJJButton() {
 function CreateModel() {
 	var owidth = window.document.body.offsetWidth - 200;
 	var oheight = window.document.body.offsetHeight - 100;
-	var ret = window.open(AppUrl +'page/PM_091105/index.html?V_ORDERGUID='+ $("#V_ORDERGUID").val(), '', 'height=' + oheight + ',width=' + owidth + ',top=10px,left=10px,resizable=yes');
+	var ret = window.open(AppUrl +'page/PM_091105/index.html?V_ORDERGUID='+ $("#V_ORDERGUID").val()+
+		'&V_ORGCODE='+ $("#V_ORGCODE").val()+
+		'&V_DEPTCODE='+$("#V_DEPTCODE").val()+
+		'&V_EQUCODE='+$("#V_EQUIP_NO").html(), '', 'height=' + oheight + ',width=' + owidth + ',top=10px,left=10px,resizable=yes');
 }
 
 function OnBtnLookClicked() {
