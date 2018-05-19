6bd6c7854a7d797568bcb235e9d13a2c31a88c6f
caojiayuan <caojiayuan@newangels.cn>
2018-05-19 09:07:51 +0800

    dx_97 dx_98 dx_99 定修流程所有相关页面 过程 后台

--- service/src/main/java/org/building/pm/service/cjyService.java
+++ service/src/main/java/org/building/pm/service/cjyService.java
@@ -4971,4 +4971,32 @@ public class cjyService {
         logger.info("end PRO_PM_03_PLAN_DX_SET");
         return result;
     }
+
+    public HashMap PRO_PM_03_PLAN_DX_SET_STATE(String V_V_GUID,String V_V_STATECODE) throws SQLException {
+
+        logger.info("begin PRO_PM_03_PLAN_DX_SET_STATE");
+        HashMap result = new HashMap();
+        Connection conn = null;
+        CallableStatement cstmt = null;
+        try {
+            conn = dataSources.getConnection();
+            conn.setAutoCommit(false);
+            cstmt = conn.prepareCall("{call PRO_PM_03_PLAN_DX_SET_STATE" + "(:V_V_GUID,:V_V_STATECODE,:V_INFO)}");
+
+            cstmt.setString("V_V_GUID", V_V_GUID);
+            cstmt.setString("V_V_STATECODE", V_V_STATECODE);
+
+            cstmt.registerOutParameter("V_INFO", OracleTypes.VARCHAR);
+            cstmt.execute();
+            result.put("V_INFO",  cstmt.getObject("V_INFO"));
+        } catch (SQLException e) {
+            logger.error(e);
+        } finally {
+            cstmt.close();
+            conn.close();
+        }
+        logger.debug("result:" + result);
+        logger.info("end PRO_PM_03_PLAN_DX_SET_STATE");
+        return result;
+    }
 }
