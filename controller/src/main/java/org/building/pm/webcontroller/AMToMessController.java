75f30e61b73c514488ec218cc0f6def9169234bf
张迹海 <zhangjihai@newangels.cn>
2018-06-04 16:33:54 +0800

    即时通修改

--- controller/src/main/java/org/building/pm/webcontroller/AMToMessController.java
+++ controller/src/main/java/org/building/pm/webcontroller/AMToMessController.java
@@ -1,73 +1,51 @@
 package org.building.pm.webcontroller;
 
-import org.springframework.beans.factory.annotation.Value;
+
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Service;
+import org.apache.axis.encoding.XMLType;
+import org.apache.axis.message.SOAPHeaderElement;
+import org.building.pm.service.BasicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.w3c.dom.Document;
 
 import javax.xml.namespace.QName;
-import javax.xml.soap.*;
-import javax.xml.ws.Dispatch;
-import javax.xml.ws.Service;
-import java.net.URL;
+import javax.xml.rpc.ParameterMode;
+import javax.xml.soap.SOAPException;
 
 @Component
 public class AMToMessController {
-    public String AMToMess(String xele, String Url, String infopuburl, String infopubusername, String infopubpassword)
-
-            throws Exception {
 
 
-        String ns = "http://hoteamsoft.org/T";  // {1}
-        String wsdlUrl = infopuburl;  // {2}
-        String username = infopubusername;
-        String password = infopubpassword;
-        //1、创建服务(Service)
-        URL url = new URL(wsdlUrl);
-        QName sname = new QName(ns, "AMToMess"); // {3}
-        Service service = Service.create(url, sname);
+    public String AMToMess(String xele, String Url, String infopuburl, String infopubusername, String infopubpassword) throws Exception {
 
-        //2、创建Dispatch
-        Dispatch<SOAPMessage> dispatch = service.createDispatch(new QName(ns, "AMToMessSoap12"), SOAPMessage.class, Service.Mode.MESSAGE); // {4}
+        try {
+            Service service = new Service();
+            Call call = (Call) service.createCall();
+            call.setTargetEndpointAddress(new java.net.URL(infopuburl));
+            call.setSOAPActionURI("http://hoteamsoft.org/AMToMessIFCheck");
+            call.setOperationName(new QName("http://hoteamsoft.org/T", "AMToMessIFCheck"));
+            call.addParameter("xele", XMLType.XSD_STRING, ParameterMode.IN);
+            call.addParameter("url", XMLType.XSD_STRING, ParameterMode.IN);
 
-        //3、创建SOAPMessage
-        SOAPMessage msg = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL).createMessage();
-        SOAPEnvelope envelope = msg.getSOAPPart().getEnvelope();
-        SOAPBody body = envelope.getBody();
+            SOAPHeaderElement soapHeaderElement = new SOAPHeaderElement(
+                    "http://hoteamsoft.org/T", "MySoapHeader");
+            soapHeaderElement.setNamespaceURI("http://hoteamsoft.org/T");
+            try {
+                soapHeaderElement.addChildElement("UserName").setValue(infopubusername); //业务系统用户名
+                soapHeaderElement.addChildElement("PassWord").setValue(infopubpassword); //业务系统密码
+            } catch (SOAPException e) {
+                e.printStackTrace();
+            }
+            call.addHeader(soapHeaderElement);
 
-        //3.2、处理header信息
-        SOAPHeader header = envelope.getHeader();
-        if (header == null) header = envelope.addHeader();
+            call.setReturnType(XMLType.XSD_STRING);
 
-
-
-
-        QName hname = new QName(ns, "AMToMessIFCheck", "t");
-        SOAPHeaderElement sheader = header.addHeaderElement(hname);
-       sheader.addChildElement("UserName").setValue(username);
-        sheader.addChildElement("PassWord").setValue(password);
-
-
-        //4、创建QName来指定消息中传递数据
-        QName ename = new QName(ns, "AMToMessIFCheck", "t");//<nn:add xmlns="xx"/>  // {5}
-        SOAPBodyElement ele = body.addBodyElement(ename);
-        // 传递参数
-        ele.addChildElement("xele").setValue(xele);
-        ele.addChildElement("url").setValue(Url);
-        msg.writeTo(System.out);
-        // System.out.println("\n invoking.....");
-
-        //5、通过Dispatch传递消息,会返回响应消息
-        SOAPMessage response = dispatch.invoke(msg);
-        response.writeTo(System.out);
-        //System.out.println();
-
-        //6、响应消息处理,将响应的消息转换为dom对象
-        Document doc = response.getSOAPPart().getEnvelope().getBody().extractContentAsDocument();
-        String str = doc.getElementsByTagName("AMToMessIFCheckResult").item(0).getTextContent();  // {7}
-        //System.out.println(str);
-
-
-        return str;
-
+            String ret = (String) call.invoke(new Object[]{xele, Url});//xele为主体发送参数（详见“业务系统发送信息服务模板参数说明”）;url为推送到终端并能访问业务系统的链接地址。
+            return ret;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Fail";
+        }
     }
 }
