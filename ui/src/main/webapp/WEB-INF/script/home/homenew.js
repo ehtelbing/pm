79657be5790749f7fdf1768620ac17f325b58487
王世一 <wangshiyi@newangels.cn>
2018-08-14 09:05:55 +0800

    首页公告

--- ui/src/main/webapp/WEB-INF/script/home/homenew.js
+++ ui/src/main/webapp/WEB-INF/script/home/homenew.js
@@ -1,14 +1,48 @@
+var noticeStore = Ext.create("Ext.data.Store", {
+    autoLoad: true,
+    storeId: 'noticeStore',
+    fields: [],
+    proxy: {
+        type: 'ajax',
+        async: false,
+        url: AppUrl + '',//需要存储过程！
+        actionMethods: {
+            read: 'POST'
+        },
+        reader: {
+            type: 'json',
+            root: 'list'
+        },
+        extraParams: {}
+    },
+    listeners: {
+        load: function (store) {
+//            notice();调用通知列表
+        }
+    }
+});
 $(function () {
     Ext.getBody().mask('<p>页面载入中...</p>');//页面笼罩效果
+//    notice();
     _AgencySelect();
     _QXNumSelect();
-
     QuerySumDb();
     JHselect();
-
-    setInterval("QuerySumDb()",120000);
-    setInterval("_QXNumSelect()",120000);
+    setInterval("QuerySumDb()", 120000);
+    setInterval("_QXNumSelect()", 120000);
+    setInterval("noticeUp('.notice ul','-35px',500)", 2000);
 });
+
+//通过数据库返回数据生成通知列表
+function notice() {
+    var s1 = '<ul>\n';
+    for (var i = 0; i < noticeStore.length; i++) {
+        var record = noticeStore.getAt(i).get();
+        s1 = s1 + '<li><a href=' + record.data.aaa + '><span></span><img src="../../css/home/images/bullet_blue.png" width="16" height="16" alt=""/>' + record.data.bbb + '<i>' + record.data.ccc + '</i></a></li>\n';
+    }
+    s1 = s1 + '</ul>';
+    $("#notice").append(s1);
+}
 
 function QuerySumDb() {
     Ext.Ajax.request({
@@ -25,11 +59,9 @@ function QuerySumDb() {
             }
         }
     });
-
 }
 
 function GoToDb() {
-
     window.parent.append("PM_0101", "流程待办", AppUrl + 'page/PM_2103/index.html');
 }
 
@@ -42,12 +74,10 @@ function tabreload() {
 function getWeekOfMonth() {//周一为起始
     var w = new Date().getDay() == 0 ? 7 : new Date().getDay();//星期
     var d = new Date().getDate();//日期
-
     var week = Math.ceil((d + 7 - w) / 7);//向上取整
-
     return week;
-
 };
+
 function JHselect() {
     $.ajax({
         type: 'POST',
@@ -67,6 +97,7 @@ function JHselect() {
         }
     });
 }
+
 function _QXNumSelect() {
     $.ajax({
         type: 'POST',
@@ -77,15 +108,12 @@ function _QXNumSelect() {
             X_PERSONCODE: Ext.util.Cookies.get('v_personcode')
         },
         success: function (data) {
-
             Ext.fly('wclqxcount').dom.innerHTML = '（' + data.total + '）';
-
             Ext.getBody().unmask();//去除页面笼罩
         }
     });
-
-
 }
+
 function _AgencySelect() {
     $('#t1').empty();
     $.ajax({
@@ -98,11 +126,9 @@ function _AgencySelect() {
         },
         success: function (data) {
             if (data.success) {
-
                 var formList = data.list;
                 var length = 0;
                 var yangshi = "onmouseover=\"this.style.backgroundPosition='left -40px'\"";
-
                 var yangshi2 = "onmouseout=\"this.style.backgroundPosition='left top'\"";
                 if (formList.length >= 3) {
                     length = 3;
@@ -112,12 +138,9 @@ function _AgencySelect() {
                 for (var i = 0; i < length; i++) {
                     $('<ul class="tasklist"> <li><span> <input type="button" name="button" id="button" class="btns" value="确认办理" ' + yangshi + ' ' + yangshi2 + 'onclick="_banli(\'' + formList[i].V_TIMER_GUID + '\')"> </span>您有' + formList[i].NUM + '条代办任务需要办理... <i>' + formList[i].V_TIMER_TIME.substring(0, 19) + '</i> </li> </ul>').appendTo('#t1');
                 }
-
-
             } else {
                 alert(data.message);
             }
-
             Ext.getBody().unmask();//去除页面笼罩
         }
     });
@@ -177,14 +200,12 @@ function _banli(V_TIMER_GUID) {
     var owidth = window.screen.availWidth;
     var oheight = window.screen.availHeight - 50;
     window.open(AppUrl + 'page/PM_060106/index.html?V_TIMER_GUID=' + V_TIMER_GUID, '', 'height=' + oheight + 'px,width= ' + owidth + 'px,top=50px,left=100px,resizable=yes');
-
 }
 
 function toYearPlan() {
     var container = top.Ext.getCmp('container');
     var url = AppUrl + 'page/PM_220101/index.html';
     var n = container.getComponent("njh");
-
     if (!n) {
         n = container.add({
             id: 'njh',
@@ -203,7 +224,6 @@ function toQuarterPlan() {
     var container = top.Ext.getCmp('container');
     var url = AppUrl + 'page/PM_03010101/index.html';
     var n = container.getComponent("jdjh");
-
     if (!n) {
         n = container.add({
             id: 'jdjh',
@@ -222,7 +242,6 @@ function toMonthPlan() {
     var container = top.Ext.getCmp('container');
     var url = AppUrl + 'page/PM_03010201/index.html';
     var n = container.getComponent("yjh");
-
     if (!n) {
         n = container.add({
             id: 'yjh',
@@ -241,7 +260,6 @@ function toWeekPlan() {
     var container = top.Ext.getCmp('container');
     var url = AppUrl + 'page/PM_03010301/index.html';
     var n = container.getComponent("zjh");
-
     if (!n) {
         n = container.add({
             id: 'zjh',
@@ -260,7 +278,6 @@ function toDailyCheck() {
     var container = top.Ext.getCmp('container');
     var url = AppUrl + 'page/PM_060105/index.html';
     var n = container.getComponent("rcdj");
-
     if (!n) {
         n = container.add({
             id: 'rcdj',
@@ -279,7 +296,6 @@ function toDefectManage() {
     var container = top.Ext.getCmp('container');
     var url = AppUrl + 'page/PM_0702/index.html';
     var n = container.getComponent("qxgl");
-
     if (!n) {
         n = container.add({
             id: 'qxgl',
@@ -298,7 +314,6 @@ function toOrderCreate() {
     var container = top.Ext.getCmp('container');
     var url = AppUrl + 'page/PM_0901/index.html';
     var n = container.getComponent("gdcj");
-
     if (!n) {
         n = container.add({
             id: 'gdcj',
@@ -313,12 +328,10 @@ function toOrderCreate() {
     container.setActiveTab(n);
 }
 
-
 function toFixOldManager() {
     var container = top.Ext.getCmp('container');
     var url = AppUrl + 'page/PM_0920/index.html';
     var n = container.getComponent("xjgl");
-
     if (!n) {
         n = container.add({
             id: 'xjgl',
@@ -333,7 +346,6 @@ function toFixOldManager() {
     container.setActiveTab(n);
 }
 
-
 function OnPageLoad() {
     Ext.Ajax.request({
         url: AppUrl + 'basic/PM_PRO_DB_PERSONNUM_SEL',
@@ -346,13 +358,10 @@ function OnPageLoad() {
         },
         success: function (ret) {
             var resp = Ext.JSON.decode(ret.responseText);
-
             Ext.getCmp('db').setValue(resp.V_DBNUM + '条');
             Ext.getCmp('yb').setValue(resp.V_YBNUM + '条');
-
         }
     });
-
     Ext.Ajax.request({
         url: AppUrl + 'basic/PM_PRO_DEFECT_PERSONNUM_SEL',
         method: 'POST',
@@ -364,16 +373,13 @@ function OnPageLoad() {
         },
         success: function (ret) {
             var resp = Ext.JSON.decode(ret.responseText);
-
             Ext.getCmp('wclqx').setValue(resp.V_WCL_NUM + '条');
             Ext.getCmp('yxpqx').setValue(resp.V_YXP_NUM + '条');
             Ext.getCmp('yxqqx').setValue(resp.V_YCL_NUM + '条');
             Ext.getCmp('sgxqqx').setValue(resp.V_SGXQ_NUM + '条');
             Ext.getCmp('ylqx').setValue(resp.V_YL_NUM + '条');
-
         }
     });
-
     Ext.Ajax.request({
         url: AppUrl + 'basic/PM_PRO_PLAN_PERSONNUM_SEL',
         method: 'POST',
@@ -385,12 +391,10 @@ function OnPageLoad() {
         },
         success: function (ret) {
             var resp = Ext.JSON.decode(ret.responseText);
-
             Ext.getCmp('njh').setValue(resp.V_YEAR_NUM + '条');
             Ext.getCmp('jjh').setValue(resp.V_QUARTER_NUM + '条');
             Ext.getCmp('yjh').setValue(resp.V_MONTH_NUM + '条');
             Ext.getCmp('zjh').setValue(resp.V_WEEK_NUM + '条');
-
         }
     });
     Ext.Ajax.request({//工单
@@ -404,34 +408,29 @@ function OnPageLoad() {
         },
         success: function (ret) {
             var resp = Ext.JSON.decode(ret.responseText);
-
             Ext.getCmp('cjgd').setValue(resp.V_CJ_NUM + '条');
             Ext.getCmp('jsgd').setValue(resp.V_JS_NUM + '条');
             Ext.getCmp('fkgd').setValue(resp.V_FK_NUM + '条');
             Ext.getCmp('ysgd').setValue(resp.V_YS_NUM + '条');
             Ext.getCmp('ylgd').setValue(resp.V_YL_NUM + '条');
-
         }
     });
-
-
 }
+
 function dealWith(URL, V_ORDERID, V_DBGUID, V_ORDERGUID, V_FLOWSTEP, V_FLOWTYPE) {
     checktabid = parseInt(Ext.getCmp('tabpanel').getActiveTab().id.substring(8));
     var owidth = window.document.body.offsetWidth - 800;
     var oheight = window.document.body.offsetHeight - 200;
     if (V_FLOWTYPE == 'WORK') {
         window.open(AppUrl + "page" + URL + "?V_ORDERID=" + V_ORDERID + "&V_DBGUID=" + V_DBGUID + "&V_ORDERGUID=" + V_ORDERGUID + "&V_FLOWSTEP=" + V_FLOWSTEP,
-            "", 'height=' + 600 + ',width=' + 1000 + ',top=10px,left=10px,resizable=no');
+                "", 'height=' + 600 + ',width=' + 1000 + ',top=10px,left=10px,resizable=no');
     } else {
         window.open(AppUrl + "page" + URL + "?V_GUID=" + V_ORDERGUID + '&random=' + Math.random(),
-            "", 'height=' + 600 + ',width=' + 1000 + ',top=10px,left=10px,resizable=no');
+                "", 'height=' + 600 + ',width=' + 1000 + ',top=10px,left=10px,resizable=no');
     }
-
-
 }
-function addEdit(id) {
 
+function addEdit(id) {
     Ext.data.StoreManager.lookup('gridWinStore').load({
         params: {
             V_V_ORDERID: id,
@@ -448,6 +447,7 @@ function Query() {
     Ext.data.StoreManager.lookup('gridStore').load();
     Ext.ComponentManager.get("tabpanel").setActiveTab(tabIndex);
 }
+
 function rendererTime(value, metaData) {
     //return Ext.Date.format(value, 'Y-m-d');
     return value.split('.0')[0];
@@ -456,14 +456,24 @@ function rendererTime(value, metaData) {
 function toDownloadChrome() {
     location.href = AppUrl + "/resources/Chrome28.exe";
 }
+
 function toDownloadLodop() {
     location.href = AppUrl + "/resources/install_lodop32.exe";
 }
+
 function toDownloadLodop32() {
     location.href = AppUrl + "/resources/CLodop_Setup_for_Win32NT.exe";
 }
+
 function toDownloadLodop64() {
     location.href = AppUrl + "/resources/CLodop_Setup_for_Win64NT_3.037Extend.exe";
 }
 
+function noticeUp(obj, top, time) {
+    $(obj).animate({
+        marginTop: top
+    }, time, function () {
+        $(this).css({marginTop: "0"}).find(":first").appendTo(this);
+    })
+}
 
