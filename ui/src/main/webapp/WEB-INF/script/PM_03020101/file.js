ca69b9e2e393d03c898cfa9395b4e168f15222a7
hanmingcheng <12345678>
2018-10-24 15:04:45 +0800

    修改附件上传

--- ui/src/main/webapp/WEB-INF/script/PM_03020101/file.js
+++ ui/src/main/webapp/WEB-INF/script/PM_03020101/file.js
@@ -8,10 +8,10 @@ Ext.onReady(function() {
     var gridStore = Ext.create('Ext.data.Store', {
         id: 'gridStore',
         autoLoad: false,
-        fields:['ID','V_GUID','V_FILEGUID','V_FILENAME','V_INPERCODE','V_INPERNAME','V_TYPE','V_INTIME','V_FILETYPE'],
+        fields:['ID','V_GUID','V_FILEGUID','V_FILENAME','V_INPERCODE','V_INPERNAME','V_TYPE','V_INTIME','V_FILETYPE','FNAME'],
         proxy: {
             type: 'ajax',
-            url: AppUrl+'PM_03/PM_03_PLAN_PROJECT_FILE_SEL',
+            url: AppUrl+'dxfile/PM_03_PLAN_PROJECT_FILE_SEL2',
             actionMethods: {
                 read: 'POST'
             },
@@ -20,8 +20,35 @@ Ext.onReady(function() {
                 root: 'list'
             }
         }
-    })
-
+    });
+    var ftypeStore=Ext.create('Ext.data.Store',{
+       id:'ftypeStore',
+       autoLoad:false,
+       fields:['ID','FNAME','FTYPE'],
+       proxy:{
+           type:'ajax',
+            url:AppUrl+'dxfile/FILETYPE_GETLIST',
+            actionMethods:{
+               read:'POST'
+            },
+            reader:{
+               type:'json',
+               root:'list'
+            }
+        }
+    });
+    Ext.data.StoreManager.lookup('ftypeStore').load({
+        params:{
+            SIGN:'D'
+        }
+    });
+    Ext.data.StoreManager.lookup('ftypeStore').on('load',function(){
+        Ext.ComponentManager.get('f_type').store.insert(0,{
+            'ID':'%',
+            'FNAME':'全部'
+        });
+       Ext.getCmp('f_type').select(Ext.data.StoreManager.lookup('ftypeStore').data.getAt(0));
+    });
     var panel = Ext.create('Ext.Panel', {
         id :'panel',
         region: 'north',
@@ -39,18 +66,36 @@ Ext.onReady(function() {
             baseCls : 'my-panel-no-border',
             frame : true,
             width : '100%',
-            items : [{
+            items : [{xtype:'combobox',
+                     id:'f_type',
+                     store:ftypeStore,
+                     fieldLabel:'附件类型',
+                     valueField:'ID',
+                     displayField:'FNAME',
+                     width:260,
+                     labelAlign:'right',
+                     queryMode:'local',
+                      editable:false,
+                     style : ' margin: 5px 2px 5px 5px',
+                listeners:{
+                select:function(){
+                    gridload();
+                }
+                }
+            },
+                {
                 xtype : 'filefield',
                 id : 'upload',
                 name : 'upload',
                 fieldLabel : '文件上传',
                 labelAlign:'right',
+                labelWidth:80,
                 width : 300,
                 msgTarget : 'side',
                 allowBlank : true,
                 anchor : '100%',
                 buttonText : '浏览....',
-                style : ' margin: 5px 0px 5px 10px'
+                style : ' margin: 5px 0px 5px 3px'
             }, {
                 xtype : 'button',
                 width : 60,
@@ -58,6 +103,9 @@ Ext.onReady(function() {
                 style : ' margin: 5px 0px 0px 10px',
                 handler : function () {
                     var formpanel = Ext.getCmp('formpanel');
+                    if(Ext.getCmp('f_type').getValue()=="%"){
+                        { Ext.Msg.alert('提示信息','请选择上传附件类型'); return}
+                    }
                     if(Ext.getCmp('upload').getValue()==''||Ext.getCmp('upload').getValue()==null||Ext.getCmp('upload').getValue()==undefined){
                         Ext.Msg.alert('提示信息', '请选择要的上传文件');
                         return;
@@ -70,7 +118,7 @@ Ext.onReady(function() {
                                 V_V_GUID:guid,
                                 V_V_INPERCODE:Ext.util.Cookies.get('v_personcode'),
                                 V_V_INPERNAME:Ext.util.Cookies.get('v_personname2'),
-                                V_V_TYPE:type
+                                V_V_TYPE:Ext.getCmp('f_type').getValue()  //type
                             },
                             success: function (response) {
                                 gridload();
@@ -78,7 +126,16 @@ Ext.onReady(function() {
                         });
                     }
                 }
-            }]
+            },{
+                    xtype:'label',
+                    text:'附件数量:',
+                    id:'bq',
+                    width:80,
+                    style:'margin:8px 5px 5px 0px;text-align:right;'
+                },{xtype:'label',
+                    id:'fjsl',
+                    width:30,
+                    style:'margin:9px 2px 5px 5px'}]
         }, {
             xtype: 'button',
             text: '刷新',
@@ -111,7 +168,10 @@ Ext.onReady(function() {
         },
         columns: [{xtype: 'rownumberer', text: '序号', width : 50,align:'center'},
             {text:'文件名称', dataIndex:'V_FILENAME', align:'center', width:260},
-            {text:'上传时间',dataIndex:'V_INTIME', align:'center', width:170}
+            {text:'上传时间',dataIndex:'V_INTIME', align:'center', width:170,renderer:function(value, metaData, record, rowIdx, colIdx, store, view){
+            return value.toString().substring(0,10);
+    }},
+            {text:'附件类型',dataIndex:'FNAME',align:'center',width:100}
         ]
     });
 
@@ -171,7 +231,10 @@ function gridload(){
             V_V_GUID:guid,
             V_V_FILEGUID:'',
             V_V_FILENAME:'',
-            V_V_TYPE:type
+            V_V_TYPE:Ext.getCmp('f_type').getValue() //type
         }
+    });
+    Ext.data.StoreManager.lookup('gridStore').on('load',function(store, records, success, eOpts){
+        Ext.getCmp('fjsl').setText(store.getProxy().getReader().rawData.V_COUNT);
     });
 }
