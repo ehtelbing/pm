f9c424ced2639155deea214b8f649823642777f2
hanmingcheng <12345678>
2019-04-17 17:10:39 +0800

    --

--- controller/src/main/java/org/building/pm/webcontroller/MobileController.java
+++ controller/src/main/java/org/building/pm/webcontroller/MobileController.java
@@ -1,8 +1,15 @@
 package org.building.pm.webcontroller;
 
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.history.HistoricVariableInstance;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import net.sf.json.JSON;
 import net.sf.json.JSONArray;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.identity.User;
 import org.building.pm.controller.CjyController;
 import org.building.pm.service.CjyService;
 import org.building.pm.service.CxyService;
@@ -29,6 +36,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
 import java.net.URLEncoder;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Blob;
@@ -60,6 +68,15 @@ public class MobileController {
 
     @Autowired
     private ZdhService zdhService;
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private HistoryService historyService;
+
+    @Autowired
+    private IdentityService identityService;
+    @Autowired
 
     /*
      * mes移动端--登录
@@ -242,6 +259,166 @@ public class MobileController {
         out.print(json.toString());
         out.close();
     }
+
+    /*
+    update 2019/04/17
+    */
+    /*事故独立工单下达*/
+    @RequestMapping(value="MobileFSaveWork",method=RequestMethod.GET)
+    @ResponseBody
+    public void MobileFSaveWork(@RequestParam(value="V_V_PERCODE") String V_V_PERCODE,
+                                @RequestParam(value="V_V_PERNAME") String V_V_PERNAME,
+                                @RequestParam(value="V_V_EQUCODE") String V_V_EQUCODE,
+                                @RequestParam(value="V_V_ORGCODE") String V_V_ORGCODE,
+                                @RequestParam(value="V_V_DEPTCODE") String V_V_DEPTCODE,
+                                @RequestParam(value="V_V_WORKORDER_TYPE") String V_V_WORKORDER_TYPE,
+                                @RequestParam(value="V_V_ORDERGUID") String V_V_ORDERGUID,
+                                @RequestParam(value="V_V_SHORT_TXT") String V_V_SHORT_TXT,
+                                @RequestParam(value="V_V_DEPTCODEREPAIR") String V_V_DEPTCODEREPAIR,
+                                @RequestParam(value="V_D_START_DATE") String V_D_START_DATE,
+                                @RequestParam(value="V_D_FINISH_DATE") String V_D_FINISH_DATE,
+                                @RequestParam(value="V_V_WBS") String V_V_WBS,
+                                @RequestParam(value="V_V_WBS_TXT") String V_V_WBS_TXT,
+                                HttpServletRequest request,
+                                HttpServletResponse resp)throws NoSuchAlgorithmException, SQLException, IOException {
+        Map data=cxyService.PRO_PM_WORKORDER_F_SAVE(V_V_PERCODE,V_V_PERNAME,V_V_EQUCODE,V_V_ORGCODE,V_V_DEPTCODE,
+                V_V_WORKORDER_TYPE,V_V_ORDERGUID,V_V_SHORT_TXT,V_V_DEPTCODEREPAIR,V_D_START_DATE,V_D_FINISH_DATE,V_V_WBS,V_V_WBS_TXT);
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+        JSONArray json = JSONArray.fromObject(data);
+        out.print(json.toString());
+        out.close();
+    }
+
+    /*事故独立工单待办查询*/
+    @RequestMapping(value="MobileFSelectWork",method=RequestMethod.GET)
+    @ResponseBody
+    public void MobileFSelectWork(@RequestParam(value = "PersonCode") String PersonCode,
+                                  @RequestParam(value = "FlowType") String FlowType,
+                                  @RequestParam(value = "FlowCode") String FlowCode,
+                                  @RequestParam(value = "ZyType") String ZyType,
+                                  @RequestParam(value = "Page") String Page,
+                                  @RequestParam(value = "PageSize") String PageSize,
+                                  HttpServletRequest request,
+                                  HttpServletResponse resp)throws NoSuchAlgorithmException, SQLException, IOException {
+        Map result = new HashMap();
+        List resultlist = new ArrayList();
+
+        int total = 0;
+        int start = (Integer.valueOf(Page) - 1) * Integer.valueOf(PageSize);
+        int limit = Integer.valueOf(PageSize);
+        List<Task> taskList = null;
+        if (FlowCode.equals("")) {
+            if (PersonCode.equals("ActivitiManage")) {
+                if(ZyType.equals("%") || ZyType.equals("")){
+                    taskList = taskService.createTaskQuery().processVariableValueLike("flow_type", FlowType + "%").orderByTaskCreateTime().desc().listPage(start, limit);
+                    total = (int) taskService.createTaskQuery().processVariableValueLike("flow_type", FlowType + "%").count();
+                }else{
+                    taskList = taskService.createTaskQuery().processVariableValueLike("flow_type", FlowType + "%").processVariableValueLike("zyName", ZyType).orderByTaskCreateTime().desc().listPage(start, limit);
+                    total = (int) taskService.createTaskQuery().processVariableValueLike("flow_type", FlowType + "%").processVariableValueLike("zyName", ZyType).count();
+                }
+            } else {
+                if(ZyType.equals("%") || ZyType.equals("")){
+                    taskList = taskService.createTaskQuery().taskAssignee(PersonCode).processVariableValueLike("flow_type", FlowType + "%").orderByTaskCreateTime().desc().listPage(start, limit);
+                    total = (int) taskService.createTaskQuery().taskAssignee(PersonCode).processVariableValueLike("flow_type", FlowType + "%").count();
+                }else{
+                    taskList = taskService.createTaskQuery().taskAssignee(PersonCode).processVariableValueLike("flow_type", FlowType + "%").processVariableValueLike("zyName", ZyType).orderByTaskCreateTime().desc().listPage(start, limit);
+                    total = (int) taskService.createTaskQuery().taskAssignee(PersonCode).processVariableValueLike("flow_type", FlowType + "%").processVariableValueLike("zyName", ZyType).count();
+                }
+
+            }
+        } else {
+            if (PersonCode.equals("ActivitiManage")) {
+                if(ZyType.equals("%") || ZyType.equals("")){
+                    taskList = taskService.createTaskQuery().processVariableValueLike("flow_code", "%" + FlowCode + "%").processVariableValueLike("flow_type", FlowType + "%").orderByTaskCreateTime().desc().listPage(start, limit);
+                    total = (int) taskService.createTaskQuery().processVariableValueLike("flow_code", "%" + FlowCode + "%").processVariableValueLike("flow_type", FlowType + "%").count();
+                }else{
+                    taskList = taskService.createTaskQuery().processVariableValueLike("flow_code", "%" + FlowCode + "%").processVariableValueLike("flow_type", FlowType + "%").processVariableValueLike("zyName", ZyType).orderByTaskCreateTime().desc().listPage(start, limit);
+                    total = (int) taskService.createTaskQuery().processVariableValueLike("flow_code", "%" + FlowCode + "%").processVariableValueLike("flow_type", FlowType + "%").processVariableValueLike("zyName", ZyType).count();
+                }
+
+            } else {
+                if(ZyType.equals("%") || ZyType.equals("")){
+                    taskList = taskService.createTaskQuery().taskAssignee(PersonCode).processVariableValueLike("flow_code", "%" + FlowCode + "%").processVariableValueLike("flow_type", FlowType + "%").orderByTaskCreateTime().desc().listPage(start, limit);
+                    total = (int) taskService.createTaskQuery().taskAssignee(PersonCode).processVariableValueLike("flow_code", "%" + FlowCode + "%").processVariableValueLike("flow_type", FlowType + "%").count();
+                }else{
+                    taskList = taskService.createTaskQuery().taskAssignee(PersonCode).processVariableValueLike("flow_code", "%" + FlowCode + "%").processVariableValueLike("flow_type", FlowType + "%").processVariableValueLike("zyName", ZyType).orderByTaskCreateTime().desc().listPage(start, limit);
+                    total = (int) taskService.createTaskQuery().taskAssignee(PersonCode).processVariableValueLike("flow_code", "%" + FlowCode + "%").processVariableValueLike("flow_type", FlowType + "%").processVariableValueLike("zyName", ZyType).count();
+                }
+
+            }
+        }
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (Task task : taskList) {
+            Map taskmap = new HashMap();
+            taskmap.put("Id", task.getId());
+            taskmap.put("Name", task.getName());
+            taskmap.put("ExecutionId", task.getExecutionId());
+            taskmap.put("Description", task.getDescription());
+            taskmap.put("ProcessInstanceId", task.getProcessInstanceId());
+            taskmap.put("ProcessDefinitionId", task.getProcessDefinitionId());
+            taskmap.put("Assignee", task.getAssignee());
+            taskmap.put("CreateTime", dateFormat.format(task.getCreateTime()));
+            taskmap.put("TaskDefinitionKey", task.getTaskDefinitionKey());
+
+            // BusinessKey
+            ProcessInstance instance = runtimeService
+                    .createProcessInstanceQuery()
+                    .processInstanceId(task.getProcessInstanceId())
+                    .singleResult();
+
+            taskmap.put("BusinessKey", instance.getBusinessKey());
+            taskmap.put("ProcessDefinitionName", instance.getProcessDefinitionName());
+            taskmap.put("ProcessDefinitionKey", instance.getProcessDefinitionKey());
+
+
+            // ProcessVariables
+            List<HistoricVariableInstance> vars = historyService
+                    .createHistoricVariableInstanceQuery()
+                    .processInstanceId(instance.getId()).list();
+            for (HistoricVariableInstance var : vars) {
+                taskmap.put(var.getVariableName(), var.getValue());
+            }
+
+            if (taskmap.get("flow_type").toString().indexOf("Fault")!=-1) {
+//                    equIp_name = (List) cxyService.PRO_FAULT_ITEM_DATA_GET(taskmap.get("BusinessKey").toString()).get("list");
+                List<Map> equIp_name = (List) cxyService.PRO_FAULT_ITEM_DATA_GET(taskmap.get("BusinessKey")==null?"":taskmap.get("BusinessKey").toString()).get("list");
+                if (equIp_name.size() > 0) {
+                    Map equmap = (Map) equIp_name.get(0);
+                    taskmap.put("EQUNAME", equmap.get("V_EQUNAME").toString());
+                    taskmap.put("PLANSTART", equmap.get("V_FINDTIME").toString());
+                    taskmap.put("PLANEND", equmap.get("V_ENDTIME").toString());
+                    taskmap.put("PLANHOUR", equmap.get("V_TIME").toString());
+                    taskmap.put("OPERANAME", equmap.get("V_FZR").toString());
+                    taskmap.put("ORGNAME", equmap.get("V_ORGNAME").toString());
+                    taskmap.put("DEPTNAME", equmap.get("V_DEPTNAME").toString());
+                }
+            }
+
+            User user = identityService.createUserQuery().userId(taskmap.get("originator").toString()).singleResult();
+
+            if (user != null)
+                taskmap.put("startName", user.getFirstName());
+            else {
+                taskmap.put("startName", "未知");
+            }
+            resultlist.add(taskmap);
+        }
+        result.put("list", resultlist);
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+        JSONArray json = JSONArray.fromObject(result);
+        out.print(json.toString());
+        out.close();
+    }
+    /*
+    update end 2019/04/17
+    */
 
     /*
      * mes移动端菜单
