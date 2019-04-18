a60ca9ad7f3ae8e839dc58897ceb3ba658828370
张迹海 <zhangjihai@newangels.cn>
2019-04-18 15:48:30 +0800

    实体类修改

---
+++ controller/src/main/java/org/building/pm/webpublic/DXClass.java
@@ -0,0 +1,231 @@
+package org.building.pm.webpublic;
+
+/**
+ * Created by admin on 2018/2/27.
+ */
+public class DXClass {
+    private String I_ID;
+    private String V_ORGCODE;
+    private String V_DEPTCODE;
+    private String V_TYPE_CODE;
+    private String V_MAJOR_CODE;
+    private String V_PROJECT_CODE;
+    private String V_PROJECT_NAME;
+    private String V_WBS_CODE;
+    private String V_WBS_NAME;
+    private String V_CONTENT;
+    private Double V_BUDGET_MONEY;
+    private String V_REPAIR_DEPT;
+    private String V_FZR;
+    private String V_DATE_B;
+    private String V_DATE_E;
+    private String V_BZ;
+    private String V_FLOW_STATE;
+    private String V_INPER;
+    private String V_INTIEM;
+    private String V_FALG;
+    private String V_YEAR;
+    private String V_MONTH;
+    private String V_GUID;
+
+    public String getI_ID() {
+        return I_ID;
+    }
+
+    public void setI_ID(String M_ID) {
+        I_ID = M_ID;
+    }
+
+    public String getV_ORGCODE() {
+        return V_ORGCODE;
+    }
+
+    public void setV_ORGCODE(String v_ORGCODE) {
+        V_ORGCODE = v_ORGCODE;
+    }
+
+    public String getV_DEPTCODE() {
+        return V_DEPTCODE;
+    }
+    public void setV_DEPTCODE(String v_DEPTCODE) {
+        V_DEPTCODE = v_DEPTCODE;
+    }
+    public void setV_BUDGET_MONEY(Double v_BUDGET_MONEY) {
+        V_BUDGET_MONEY = v_BUDGET_MONEY;
+    }
+
+    public Double getV_BUDGET_MONEY() {
+
+        return V_BUDGET_MONEY;
+    }
+
+    public String getV_YEAR() {
+        return V_YEAR;
+    }
+
+    public void setV_YEAR(String v_YEAR) {
+        V_YEAR = v_YEAR;
+    }
+
+    public String getV_WBS_NAME() {
+
+        return V_WBS_NAME;
+    }
+
+    public void setV_WBS_NAME(String v_WBS_NAME) {
+        V_WBS_NAME = v_WBS_NAME;
+    }
+
+    public String getV_WBS_CODE() {
+
+        return V_WBS_CODE;
+    }
+
+    public void setV_WBS_CODE(String v_WBS_CODE) {
+        V_WBS_CODE = v_WBS_CODE;
+    }
+
+    public String getV_TYPE_CODE() {
+
+        return V_TYPE_CODE;
+    }
+
+    public void setV_TYPE_CODE(String v_TYPE_CODE) {
+        V_TYPE_CODE = v_TYPE_CODE;
+    }
+
+    public String getV_REPAIR_DEPT() {
+
+        return V_REPAIR_DEPT;
+    }
+
+    public void setV_REPAIR_DEPT(String v_REPAIR_DEPT) {
+        V_REPAIR_DEPT = v_REPAIR_DEPT;
+    }
+
+    public String getV_PROJECT_NAME() {
+
+        return V_PROJECT_NAME;
+    }
+
+    public void setV_PROJECT_NAME(String v_PROJECT_NAME) {
+        V_PROJECT_NAME = v_PROJECT_NAME;
+    }
+
+    public String getV_PROJECT_CODE() {
+
+        return V_PROJECT_CODE;
+    }
+
+    public void setV_PROJECT_CODE(String v_PROJECT_CODE) {
+        V_PROJECT_CODE = v_PROJECT_CODE;
+    }
+
+    public String getV_MONTH() {
+
+        return V_MONTH;
+    }
+
+    public void setV_MONTH(String v_MONTH) {
+        V_MONTH = v_MONTH;
+    }
+
+    public String getV_MAJOR_CODE() {
+
+        return V_MAJOR_CODE;
+    }
+
+    public void setV_MAJOR_CODE(String v_MAJOR_CODE) {
+        V_MAJOR_CODE = v_MAJOR_CODE;
+    }
+
+    public String getV_INTIEM() {
+
+        return V_INTIEM;
+    }
+
+    public void setV_INTIEM(String v_INTIEM) {
+        V_INTIEM = v_INTIEM;
+    }
+
+    public String getV_INPER() {
+
+        return V_INPER;
+    }
+
+    public void setV_INPER(String v_INPER) {
+        V_INPER = v_INPER;
+    }
+
+    public String getV_GUID() {
+
+        return V_GUID;
+    }
+
+    public void setV_GUID(String v_GUID) {
+        V_GUID = v_GUID;
+    }
+
+    public String getV_FZR() {
+
+        return V_FZR;
+    }
+
+    public void setV_FZR(String v_FZR) {
+        V_FZR = v_FZR;
+    }
+
+    public String getV_FLOW_STATE() {
+
+        return V_FLOW_STATE;
+    }
+
+    public void setV_FLOW_STATE(String v_FLOW_STATE) {
+        V_FLOW_STATE = v_FLOW_STATE;
+    }
+
+    public String getV_FALG() {
+
+        return V_FALG;
+    }
+
+    public void setV_FALG(String v_FALG) {
+        V_FALG = v_FALG;
+    }
+
+    public String getV_DATE_E() {
+
+        return V_DATE_E;
+    }
+
+    public void setV_DATE_E(String v_DATE_E) {
+        V_DATE_E = v_DATE_E;
+    }
+
+    public String getV_DATE_B() {
+
+        return V_DATE_B;
+    }
+
+    public void setV_DATE_B(String v_DATE_B) {
+        V_DATE_B = v_DATE_B;
+    }
+
+    public String getV_CONTENT() {
+
+        return V_CONTENT;
+    }
+
+    public void setV_CONTENT(String v_CONTENT) {
+        V_CONTENT = v_CONTENT;
+    }
+
+    public String getV_BZ() {
+
+        return V_BZ;
+    }
+
+    public void setV_BZ(String v_BZ) {
+        V_BZ = v_BZ;
+    }
+}
---
+++ controller/src/main/java/org/building/pm/webpublic/FalutClass.java
@@ -0,0 +1,295 @@
+package org.building.pm.webpublic;
+
+/**
+ * Created by zjh on 2017/4/20.
+ */
+public class FalutClass {
+    private String I_ID;
+    private String V_FZR;
+    private String V_STATE;
+    private String V_FEEDBACK;
+    private String V_FINDTIME;
+    private String V_ORDERGUID;
+    private String V_ORGCODE;
+    private String V_ORGNAME;
+    private String V_DEPTCODE;
+    private String V_DEPTNAME;
+    private String V_EQUTYPECODE;
+    private String V_EQUTYPENAME;
+    private String V_EQUCODE;
+    private String V_EQUNAME;
+    private String V_EQUCHILD_CODE;
+    private String V_FAULT_GUID;
+    private String V_EQUCHILD_NAME;
+    private String V_TYPECODE;
+    private String V_TYPENAME;
+    private String V_FAULT_YY;
+    private String V_FAULT_XX;
+    private String V_FAULT_LEVEL;
+    private String V_JJBF;
+    private String V_JJ;
+    private String V_PER_CLASS;
+    private String V_WL;
+    private String V_PART;
+    private String V_MATERIAL;
+    private String V_TIME;
+    private String V_FILE_GUID;
+    private String V_GUID;
+    private String V_BZ;
+
+    public String getV_ORGCODE() {
+        return V_ORGCODE;
+    }
+
+    public void setV_ORGCODE(String v_ORGCODE) {
+        V_ORGCODE = v_ORGCODE;
+    }
+
+    public String getV_ORGNAME() {
+        return V_ORGNAME;
+    }
+
+    public void setV_ORGNAME(String v_ORGNAME) {
+        V_ORGNAME = v_ORGNAME;
+    }
+
+    public String getV_DEPTCODE() {
+        return V_DEPTCODE;
+    }
+
+    public void setV_DEPTCODE(String v_DEPTCODE) {
+        V_DEPTCODE = v_DEPTCODE;
+    }
+
+    public String getV_DEPTNAME() {
+        return V_DEPTNAME;
+    }
+
+    public void setV_DEPTNAME(String v_DEPTNAME) {
+        V_DEPTNAME = v_DEPTNAME;
+    }
+
+    public String getV_EQUTYPECODE() {
+        return V_EQUTYPECODE;
+    }
+
+    public void setV_EQUTYPECODE(String v_EQUTYPECODE) {
+        V_EQUTYPECODE = v_EQUTYPECODE;
+    }
+
+    public String getV_EQUTYPENAME() {
+        return V_EQUTYPENAME;
+    }
+
+    public void setV_EQUTYPENAME(String v_EQUTYPENAME) {
+        V_EQUTYPENAME = v_EQUTYPENAME;
+    }
+
+    public String getV_EQUCODE() {
+        return V_EQUCODE;
+    }
+
+    public void setV_EQUCODE(String v_EQUCODE) {
+        V_EQUCODE = v_EQUCODE;
+    }
+
+    public String getV_EQUNAME() {
+        return V_EQUNAME;
+    }
+
+    public void setV_EQUNAME(String v_EQUNAME) {
+        V_EQUNAME = v_EQUNAME;
+    }
+
+    public String getV_EQUCHILD_CODE() {
+        return V_EQUCHILD_CODE;
+    }
+
+    public void setV_EQUCHILD_CODE(String v_EQUCHILD_CODE) {
+        V_EQUCHILD_CODE = v_EQUCHILD_CODE;
+    }
+
+    public String getV_FAULT_GUID() {
+        return V_FAULT_GUID;
+    }
+
+    public void setV_FAULT_GUID(String v_FAULT_GUID) {
+        V_FAULT_GUID = v_FAULT_GUID;
+    }
+
+    public String getV_EQUCHILD_NAME() {
+        return V_EQUCHILD_NAME;
+    }
+
+    public void setV_EQUCHILD_NAME(String v_EQUCHILD_NAME) {
+        V_EQUCHILD_NAME = v_EQUCHILD_NAME;
+    }
+
+    public String getV_TYPECODE() {
+        return V_TYPECODE;
+    }
+
+    public void setV_TYPECODE(String v_TYPECODE) {
+        V_TYPECODE = v_TYPECODE;
+    }
+
+    public String getV_TYPENAME() {
+        return V_TYPENAME;
+    }
+
+    public void setV_TYPENAME(String v_TYPENAME) {
+        V_TYPENAME = v_TYPENAME;
+    }
+
+    public String getV_FAULT_YY() {
+        return V_FAULT_YY;
+    }
+
+    public void setV_FAULT_YY(String v_FAULT_YY) {
+        V_FAULT_YY = v_FAULT_YY;
+    }
+
+    public String getV_FAULT_XX() {
+        return V_FAULT_XX;
+    }
+
+    public void setV_FAULT_XX(String v_FAULT_XX) {
+        V_FAULT_XX = v_FAULT_XX;
+    }
+
+    public String getV_FAULT_LEVEL() {
+        return V_FAULT_LEVEL;
+    }
+
+    public void setV_FAULT_LEVEL(String v_FAULT_LEVEL) {
+        V_FAULT_LEVEL = v_FAULT_LEVEL;
+    }
+
+    public String getV_JJBF() {
+        return V_JJBF;
+    }
+
+    public void setV_JJBF(String v_JJBF) {
+        V_JJBF = v_JJBF;
+    }
+
+    public String getV_JJ() {
+        return V_JJ;
+    }
+
+    public void setV_JJ(String v_JJ) {
+        V_JJ = v_JJ;
+    }
+
+    public String getV_PER_CLASS() {
+        return V_PER_CLASS;
+    }
+
+    public void setV_PER_CLASS(String v_PER_CLASS) {
+        V_PER_CLASS = v_PER_CLASS;
+    }
+
+    public String getV_WL() {
+        return V_WL;
+    }
+
+    public void setV_WL(String v_WL) {
+        V_WL = v_WL;
+    }
+
+    public String getV_PART() {
+        return V_PART;
+    }
+
+    public void setV_PART(String v_PART) {
+        V_PART = v_PART;
+    }
+
+    public String getV_MATERIAL() {
+        return V_MATERIAL;
+    }
+
+    public void setV_MATERIAL(String v_MATERIAL) {
+        V_MATERIAL = v_MATERIAL;
+    }
+
+    public String getV_TIME() {
+        return V_TIME;
+    }
+
+    public void setV_TIME(String v_TIME) {
+        V_TIME = v_TIME;
+    }
+
+    public String getV_FILE_GUID() {
+        return V_FILE_GUID;
+    }
+
+    public void setV_FILE_GUID(String v_FILE_GUID) {
+        V_FILE_GUID = v_FILE_GUID;
+    }
+
+    public String getV_GUID() {
+        return V_GUID;
+    }
+
+    public void setV_GUID(String v_GUID) {
+        V_GUID = v_GUID;
+    }
+
+    public String getV_BZ() {
+        return V_BZ;
+    }
+
+    public void setV_BZ(String v_BZ) {
+        V_BZ = v_BZ;
+    }
+
+    public String getI_ID() {
+        return I_ID;
+    }
+
+    public void setI_ID(String i_ID) {
+        I_ID = i_ID;
+    }
+
+    public String getV_FZR() {
+        return V_FZR;
+    }
+
+    public void setV_FZR(String v_FZR) {
+        V_FZR = v_FZR;
+    }
+
+    public String getV_STATE() {
+        return V_STATE;
+    }
+
+    public void setV_STATE(String v_STATE) {
+        V_STATE = v_STATE;
+    }
+
+    public String getV_FEEDBACK() {
+        return V_FEEDBACK;
+    }
+
+    public void setV_FEEDBACK(String v_FEEDBACK) {
+        V_FEEDBACK = v_FEEDBACK;
+    }
+
+    public String getV_FINDTIME() {
+        return V_FINDTIME;
+    }
+
+    public void setV_FINDTIME(String v_FINDTIME) {
+        V_FINDTIME = v_FINDTIME;
+    }
+
+    public String getV_ORDERGUID() {
+        return V_ORDERGUID;
+    }
+
+    public void setV_ORDERGUID(String v_ORDERGUID) {
+        V_ORDERGUID = v_ORDERGUID;
+    }
+}
