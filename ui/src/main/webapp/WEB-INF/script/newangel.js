c132a9b523cae4a9809b4c540e754666c8a4dfb4
张迹海 <zhangjihai@newangels.cn>
2019-05-27 11:49:18 +0800

    单点登录解码

--- ui/src/main/webapp/WEB-INF/script/newangel.js
+++ ui/src/main/webapp/WEB-INF/script/newangel.js
@@ -1,6 +1,8 @@
-var usercodeTemp = null;
+var usercodeTemp = '';
+var ip='';
 if (location.href.split('?')[1] != null) {
-    usercodeTemp = Ext.urlDecode(location.href.split('?')[1]).USERID;
+    usercodeTemp = Ext.urlDecode(location.href.split('?')[1]).LogUser;
+    IP = Ext.urlDecode(location.href.split('?')[1]).IP;
 }
 
 function getPath() {
@@ -35,13 +37,14 @@ var dxImgPath=APP+'/app/pm/css/images';
 
 document.write('<script type="text/javascript" charset="UTF-8" src="../../../pm/resources/shared/examples.js"></script>');
 
+var b=new Base64()
+if (!(usercodeTemp == null)) {
 
-/*if (!(usercodeTemp == null)) {
     Ext.Ajax.request({
         url: AppUrl + 'info/login_dddl_n',
         params: {
-            USERID: usercodeTemp,
-            V_V_IP: '设备平台单点登陆'
+            USERID: b.decode(usercodeTemp),
+            V_V_IP: ip?"":'设备单点登录'
         }, success: function (respon) {
             var resp = Ext.decode(respon.responseText);
             if (resp.list.length > 0) {
@@ -110,7 +113,7 @@ function subSomething() {
     if(document.readyState == "complete") {
         location.href = "../../page/login/login.html";
     }
-}*/
+}
 
 // Excel
 function createElement(elName, elValue) {
@@ -393,7 +396,6 @@ function OnPageMMLogin(){
     });
 }
 
-
 window._console = window.console;//将原始console对象缓存
 
 window.console = (function (orgConsole) {
@@ -467,3 +469,106 @@ window.console = (function (orgConsole) {
     }
 
 }(window._console));
+
+function Base64() {
+    // private property
+    _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+    // public method for encoding
+    this.encode = function (input) {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+        input = _utf8_encode(input);
+        while (i < input.length) {
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+            output = output +
+                _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +
+                _keyStr.charAt(enc3) + _keyStr.charAt(enc4);
+        }
+        return output;
+    }
+
+    // public method for decoding
+    this.decode = function (input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+        while (i < input.length) {
+            enc1 = _keyStr.indexOf(input.charAt(i++));
+            enc2 = _keyStr.indexOf(input.charAt(i++));
+            enc3 = _keyStr.indexOf(input.charAt(i++));
+            enc4 = _keyStr.indexOf(input.charAt(i++));
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+            output = output + String.fromCharCode(chr1);
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
+        }
+        output = _utf8_decode(output);
+        return output;
+    }
+
+    // private method for UTF-8 encoding
+    _utf8_encode = function (string) {
+        string = string.replace(/\r\n/g, "\n");
+        var utftext = "";
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            } else if ((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            } else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+
+        }
+        return utftext;
+    }
+
+    // private method for UTF-8 decoding
+    _utf8_decode = function (utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+        while (i < utftext.length) {
+            c = utftext.charCodeAt(i);
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            } else if ((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i + 1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            } else {
+                c2 = utftext.charCodeAt(i + 1);
+                c3 = utftext.charCodeAt(i + 2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+        }
+        return string;
+    }
+}
