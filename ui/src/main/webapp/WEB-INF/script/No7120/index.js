54cf6e1905195552def1100b4564e3398ed07e67
lixiang <lixiang@newangels.cn>
2019-11-13 09:16:10 +0800

    No7116/No7117/No7118/No7119/No7120/No7128/No7129

--- ui/src/main/webapp/WEB-INF/script/No7120/index.js
+++ ui/src/main/webapp/WEB-INF/script/No7120/index.js
@@ -1,320 +1,284 @@
-var departStoreLoad = false;
-var cycleStoreLoad = false;
+var imgid = '';
 
-Ext.onReady(function () {
-    Ext.getBody().mask('<p>页面载入中...</p>');//页面笼罩效果
-
-    //作业区
-    var departStore = Ext.create('Ext.data.Store', {
-        storeId: 'departStore',
-        autoLoad: true,
-        //pageSize: -1,
-        fields: ['V_DEPTCODE', 'V_DEPTNAME'],
-        proxy: {
-            url: AppUrl + 'PM_12/PRO_BASE_DEPT_VIEW',
-            type: 'ajax',
-            actionMethods: {
-                read: 'POST'
+Ext.onReady(function() {
+    // 作业区
+    var gzpalceStore = Ext.create('Ext.data.Store', {
+        autoLoad : true,
+        storeId : 'gzpalceStore',
+        fields : [ 'V_DEPTCODE', 'V_DEPTNAME' ],
+        proxy : {
+            type : 'ajax',
+            async : false,
+            url : AppUrl + 'lx/PRO_BASE_DEPT_VIEW',
+            actionMethods : {
+                read : 'POST'
             },
-            extraParams: {
-                IS_V_DEPTCODE: Ext.util.Cookies.get('v_orgCode'),
-                IS_V_DEPTTYPE: '[主体作业区]'
+            reader : {
+                type : 'json',
+                root : 'list'
             },
-            reader: {
-                type: 'json',
-                root: 'list'
+            extraParams : {
+                IS_V_DEPTCODE : Ext.util.Cookies.get('v_orgCode'),
+                IS_V_DEPTTYPE :  '[主体作业区]'
             }
+        }
+    });
+    // 设备选择STORE
+    var sbxzStore = Ext.create('Ext.data.Store', {
+        autoLoad : false,
+        storeId : 'sbxzStore',
+        fields : [ 'EQU_DESC', 'EQU_ID' ],
+        proxy : {
+            type : 'ajax',
+            async : false,
+            url : AppUrl + 'lx/PRO_RUN7111_EQULIST',
+            actionMethods : {
+                read : 'POST'
+            },
+            reader : {
+                type : 'json',
+                root : 'list'
+            }
+        }
+    });
+
+    // 周期类型
+    var fzrStore = Ext.create('Ext.data.Store', {
+        autoLoad : false,
+        storeId : 'fzrStore',
+        fields : [ 'CYCLE_ID', 'CYCLE_DESC' ],
+        proxy : {
+            type : 'ajax',
+            async : false,
+            url : AppUrl + 'lx/PRO_RUN_CYCLE_ABLE',
+            actionMethods : {
+                read : 'POST'
+            },
+            reader : {
+                type : 'json',
+                root : 'list'
+            }
+        }
+    });
+
+    var creatpanel1 = Ext.create('Ext.form.Panel', {
+        id : 'creatpanel1',
+        style : 'margin:5px 0px 2px 2px',
+        region : 'north',
+        width : '100%',
+        //baseCls : 'my-panel-no-border',
+        defaults : {
+            style : 'margin:5px 0px 5px 10px',
+            labelAlign : 'right'
         },
-        listeners: {
-            load: function (store, records) {
-                departStoreLoad = true;
-                Ext.getCmp('A_DEPTCODE').select(store.first());
-                _init();
-            }
-        }
-    });
-
-    //设备
-    var equStore = Ext.create('Ext.data.Store', {
-        storeId: 'equStore',
-        autoLoad: false,
-        //pageSize: -1,
-        fields: ['EQU_ID', 'EQU_DESC'],
-        proxy: {
-            url: AppUrl + 'PM_12/PRO_RUN7111_EQULIST',
-            async: false,
-            type: 'ajax',
-            actionMethods: {
-                read: 'POST'
-            },
-            extraParams: {},
-            reader: {
-                type: 'json',
-                root: 'list'
-            }
+        layout : {
+            type : 'vbox'
         },
-        listeners: {
-            load: function (store, records) {
-                Ext.getCmp('A_EQUID').select(store.first());
+        items : [ {
+            xtype : 'panel',
+            region : 'center',
+            layout : 'column',
+            //baseCls : 'my-panel-no-border',
+            defaults : {
+                labelAlign : 'right',
+                labelWidth : 50
+            },
+            items : [ {
+                xtype : 'combo',
+                id : 'zyq',
+                store : gzpalceStore,
+                fieldLabel : '作业区 ',
+                editable : false,
+                style : 'margin:5px 0px',
+                queryMode : 'local',
+                valueField : 'V_DEPTCODE',
+                displayField : 'V_DEPTNAME'
+            }, {
+                xtype : 'combo',
+                id : 'xzsb',
+                store : sbxzStore,
+                fieldLabel : '设备 ',
+                editable : false,
+                style : 'margin:5px 0px',
+                queryMode : 'local',
+                valueField : 'EQU_ID',
+                displayField : 'EQU_DESC',
+                labelWidth : 45
+            }, {
+                xtype : 'combo',
+                id : 'fzr',
+                store : 'fzrStore',
+                fieldLabel : '周期类型 ',
+                editable : false,
+                style : 'margin:5px  0px 5px 5px',
+                queryMode : 'local',
+                valueField : 'CYCLE_ID',
+                displayField : 'CYCLE_DESC',
+                labelWidth : 65
+            }, {
+                xtype : 'button',
+                text : '查询',
+                icon : imgpath + '/search.png',
+                width : 60,
+                handler : query,
+                style : {
+                    margin : '5px 0px 10px 10px'
+                }
             }
-        }
+
+            ]
+        } ]
     });
 
-    //周期类型
-    var cycleStore = Ext.create('Ext.data.Store', {
-        storeId: 'cycleStore',
-        autoLoad: true,
-        //pageSize: -1,
-        fields: ['CYCLE_ID', 'CYCLE_DESC'],
-        proxy: {
-            url: AppUrl + 'PM_12/PRO_RUN_CYCLE_ABLE',
-            async: false,
-            type: 'ajax',
-            actionMethods: {
-                read: 'POST'
-            },
-            extraParams: {},
-            reader: {
-                type: 'json',
-                root: 'list'
-            }
-        },
-        listeners: {
-            load: function (store, records) {
-                cycleStoreLoad = true;
-                Ext.getCmp('A_CYCLE_ID').select(store.first());
-                _init();
-            }
-        }
+    var store1 = Ext.create('Ext.data.JsonStore', {
+        fields : [ 'name', '报警值', '作业量' ]
+
     });
 
-    //var equBJAlertStore = Ext.create('Ext.data.JsonStore', {
-    //    id: 'equBJAlertStore',
-    //    fields: ['name', '报警值', '作业量']
-    //
-    //});
-    var equBJAlertStore = Ext.create('Ext.data.Store', {
-        storeId: 'equBJAlertStore',
-        autoLoad: false,
-        pageSize: -1,
-        fields: ['SITE_DESC', 'ALERT_VALUE', 'SUM_YEILD'],
-        proxy: {
-            url: AppUrl + 'PM_12/PRO_RUN_EQU_BJ_ALERT_ALL',
-            async: false,
-            type: 'ajax',
-            actionMethods: {
-                read: 'POST'
-            },
-            extraParams: {},
-            reader: {
-                type: 'json',
-                root: 'list'
-            }
-        }
-    });
     var chart = Ext.create('Ext.chart.Chart', {
-        style: 'background:#fff',
-        animate: true,
-        shadow: true,
-        store: equBJAlertStore,
-        region: 'center',
-        legend: {
-            position: 'right'
+        style : 'background:#fff',
+        animate : true,
+        shadow : true,
+        store : store1,
+        region : 'center',
+        legend : {
+            position : 'right'
         },
-        axes: [{
+        axes : [ {
             /** x轴 */
-            type: 'Numeric',//Numeric轴来展示区域序列数据
-            position: 'bottom',//Numeric轴放在图表下部
-            fields: ['ALERT_VALUE', 'SUM_YEILD'],
-            minimum: 0,
-            label: {
-                renderer: Ext.util.Format.numberRenderer('0,0')
+            type : 'Numeric',
+            position : 'bottom',
+            fields : [ '报警值', '作业量' ],
+            minimum : 0,
+            label : {
+                renderer : Ext.util.Format.numberRenderer('0,0')
             },
-            grid: true,
-            title: '作业量'//category和numeric轴都有grid集合,水平线和垂直线会覆盖图表的背景
-        }, {
-            /** 左侧分类 - y轴 */
-            type: 'Category',
-            position: 'left',//category轴放在图表左部
-            fields: ['SITE_DESC'],
-            title: ''
-        }],
-        series: [{
-            type: 'bar',
-            axis: 'bottom',
-            highlight: true,//如果设置为真，当鼠标移动到上面时，高亮。
-            //style: { width: 60 },//设置柱状宽度大小
-            tips: {//为可视化标记添加工具栏
-                trackMouse: true,
-                width: 130,
-                height: 25,
-                renderer: function (storeItem, item) {
-                    this.setTitle(item.yField + ': ' + item.value[1]);
-                }
-            },
-            label: {
-                display: 'insideEnd',//指定饼图标签的位置。包括"rotate", "middle", "insideStart", "insideEnd", "outside", "over", "under", 或者 "none"可以用来渲染标签。
-                fields: ['ALERT_VALUE', 'SUM_YEILD'],//标签要展示的字段的名称。
-                renderer: Ext.util.Format.numberRenderer('0'),
-                orientation: 'vertical',//"horizontal" 或者 "vertical"
-                color: '#444'
-            },
-            xField: 'SITE_DESC',
-            yField:  ['ALERT_VALUE', 'SUM_YEILD'],
-            title: ['报警值', '作业量']
-        }]
-    });
-
-    var buttonPanel = Ext.create('Ext.Panel', {
-        border: false,
-        frame: true,
-        region: 'north',
-        //title: '<div align="center"></div>',
-        width: '100%',
-        //height: 50,
-        items: [{
-            xtype: 'panel',
-            region: 'north',
-            layout: 'column',
-            baseCls: 'my-panel-no-border',
-            items: [{
-                xtype: 'combo',
-                id: 'A_DEPTCODE',
-                store: departStore,
-                editable: false,
-                queryMode: 'local',
-                fieldLabel: '作业区',
-                displayField: 'V_DEPTNAME',
-                valueField: 'V_DEPTCODE',
-                labelWidth: 80,
-                width: 250,
-                style: ' margin: 5px 0px 5px -15px',
-                labelAlign: 'right',
-                listeners: {
-                    change: function (field, newValue, oldValue) {
-                        _selectEqu();
-                    }
-                }
-            }, {
-                xtype: 'combo',
-                id: 'A_EQUID',
-                store: equStore,
-                editable: false,
-                queryMode: 'local',
-                fieldLabel: '选择设备',
-                displayField: 'EQU_DESC',
-                valueField: 'EQU_ID',
-                labelWidth: 80,
-                width: 250,
-                style: ' margin: 5px 0px 5px -5px',
-                labelAlign: 'right'
-            }, {
-                xtype: 'combo',
-                id: 'A_CYCLE_ID',
-                store: cycleStore,
-                editable: false,
-                queryMode: 'local',
-                fieldLabel: '周期类型',
-                displayField: 'CYCLE_DESC',
-                valueField: 'CYCLE_ID',
-                labelWidth: 80,
-                width: 250,
-                style: ' margin: 5px 0px 5px -5px',
-                labelAlign: 'right'
-            }, {
-                xtype: 'button',
-                text: '查询',
-                align: 'center',
-                handler: _selectEquBJAlert,
-                width: 80,
-                style: ' margin: 5px 5px 5px 30px',
-                icon: imgpath + '/search.png'
-            }]
-        }]
-    });
-
-    Ext.create('Ext.container.Viewport', {
-        layout: {
-            type: 'border',
-            regionWeights: {
-                west: -1,
-                north: 1,
-                south: 1,
-                east: -1
-            }
+            grid : true,
+            title : '作业量'
         },
-        items: [{
-            region: 'north',
-            border: false,
-            items: [buttonPanel]
-        }, {
-            region: 'center',
-            layout: 'fit',
-            border: false,
-            items: [chart]
-        }]
+
+            {
+                /** 左侧分类 - y轴 */
+                type : 'Category',
+                position : 'left',
+                fields : [ 'name' ],
+                title : ''
+            } ],
+        series : [ {
+            type : 'bar',
+            axis : 'bottom',
+            xField : 'name',
+            yField : [ '报警值', '作业量' ]
+        } ]
     });
 
-    _init();
-});
+    query();
 
-function _init() {
-    if (departStoreLoad && cycleStoreLoad) {
+    function query() {
 
-        Ext.getBody().unmask();//去除页面笼罩
+        Ext.Ajax.request({
+            async : false,
+            url : AppUrl + 'lx/PRO_RUN_EQU_BJ_ALERT_ALL',
+            method : 'POST',
+            params : {
+                A_EQUID : Ext.getCmp('xzsb').getValue(),
+                A_CYCLE_ID : Ext.getCmp('fzr').getValue()
+            },
+            success : function(response, options) {
+
+                var resp = Ext.JSON.decode(response.responseText);
+
+                var data = [];
+                Ext.Array.each(resp.list,
+                    function(name, index, countriesItSelf) {
+
+                        data.push({
+                            name : name.SITE_DESC,
+                            报警值 : name.ALERT_VALUE,
+                            作业量 : name.SUM_YEILD
+                        })
+                    });
+                store1.loadData(data);
+            }
+        });
     }
 
+    Ext.create('Ext.container.Viewport', {
+        id : "id",
+        layout : 'border',
+        items : [ creatpanel1, chart ]
+    });
+
+    /** 作业区 - 事件 */
+    gzpalceStore.on('load',
+        function() {
+            gzpalceStore.insert(0, {
+                'V_DEPTCODE' : '%',
+                'V_DEPTNAME' : '全部'
+            });
+            gzpalceStore.sort('V_DEPTCODE', 'ASC');
+            Ext.getCmp('zyq').select(gzpalceStore.getAt(0));
+            // 默认当前登录用户工作区
+            var storeDataList = gzpalceStore.data;
+            var storeLength = storeDataList.length;
+            for ( var index = 0; index < storeLength; index++) {
+                var storeItemData = storeDataList.items[index].data;
+                if (storeItemData.V_DEPTCODE == Ext.util.Cookies
+                    .get('v_deptcode')) {
+                    Ext.getCmp("zyq").setValue(
+                        Ext.util.Cookies.get('v_deptcode'));
+                    break;
+                }
+            }
+
+            /** 设备加载 */
+            Ext.data.StoreManager.lookup('sbxzStore').load(
+                {
+                    params : {
+                        v_v_plantcode : Ext.util.Cookies.get('v_orgCode'),
+                        v_v_deptcode :  Ext.getCmp("zyq").getValue()
+                    }
+                });
+        });
+
+    /** 设备 - 事件 */
+    sbxzStore.on('load', function() {
+        sbxzStore.sort('EQU_ID', 'ASC');
+        Ext.getCmp('xzsb').select(sbxzStore.getAt(0));
+    });
+
+    /** 周期类型 - 加载 */
+    Ext.data.StoreManager.lookup('fzrStore').load();
+    /** 周期类型 - 事件 */
+    Ext.data.StoreManager.lookup('fzrStore').on(
+        'load',
+        function() {
+            Ext.getCmp('fzr').store.sort('CYCLE_ID', 'ASC');
+            Ext.getCmp('fzr').select(
+                Ext.data.StoreManager.lookup('fzrStore').getAt(0));
+        });
+
+    /** 作业区 -> 设备 联动 */
+    Ext.getCmp('zyq').on(
+        'select',
+        function() {
+            Ext.data.StoreManager.lookup('sbxzStore').load(
+                {
+                    params : {
+                        v_v_plantcode : Ext.util.Cookies.get('v_orgCode'),
+                        v_v_deptcode :  Ext.getCmp("zyq").getValue()
+                    }
+                });
+        });
+
+});
+
+function RenderFontLeft(value, metaData) {
+    metaData.style = 'text-align: left';
+    value = value.split(' ')[0];
+    return value;
 }
 
-function _selectEqu() {
-    var equStore = Ext.data.StoreManager.lookup('equStore');
-    equStore.proxy.extraParams = {
-        'V_V_PLANTCODE': Ext.util.Cookies.get('v_orgCode'),
-        'V_V_DEPTCODE': Ext.getCmp('A_DEPTCODE').getValue()
-    };
-
-    equStore.load();
-}
-
-function _selectEquBJAlert() {
-    var equBJAlertStore = Ext.data.StoreManager.lookup('equBJAlertStore');
-    equBJAlertStore.proxy.extraParams = {
-        A_EQUID: Ext.getCmp('A_EQUID').getValue(),
-        A_CYCLE_ID: Ext.getCmp('A_CYCLE_ID').getValue()
-    };
-
-    equBJAlertStore.load();
-    //Ext.Ajax.request({
-    //    url: AppUrl + 'PM_12/PRO_RUN_EQU_BJ_ALERT_ALL',
-    //    type: 'ajax',
-    //    method: 'POST',
-    //    params: {
-    //        A_EQUID: Ext.getCmp('A_EQUID').getValue(),
-    //        A_CYCLE_ID: Ext.getCmp('A_CYCLE_ID').getValue()
-    //    },
-    //    success: function (response) {
-    //        var resp = Ext.decode(response.responseText);//后台返回的值
-    //        var data = [];
-    //        Ext.Array.each(resp.list,
-    //            function (name, index, countriesItSelf) {
-    //                data.push({
-    //                    name: name.SITE_DESC,
-    //                    报警值: name.ALERT_VALUE,
-    //                    作业量: name.SUM_YEILD
-    //                })
-    //            });
-    //        Ext.data.StoreManager.lookup('equBJAlertStore').loadData(data);
-    //
-    //    },
-    //    failure: function (response) {//访问到后台时执行的方法。
-    //        Ext.MessageBox.show({
-    //            title: '错误',
-    //            msg: response.responseText,
-    //            buttons: Ext.MessageBox.OK,
-    //            icon: Ext.MessageBox.ERROR
-    //        })
-    //    }
-    //
-    //});
-
+function findcode(a, record, item, index, e, eOpts) {
+    imgid = record.raw.LOGID;
 }
